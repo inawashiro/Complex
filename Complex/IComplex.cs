36a7db4f81f03ff5d6aacea9b1e2f710ffa000ab
inawashiro <hiroina0926@icloud.com>
2019-08-02 16:08:29 +0900

    feat1: Added constructors feat2: Separated the interface & classes

---
+++ Complex/IComplex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Complex
+{
+    interface IComplex
+    {
+        //properties
+        double Re { get; }
+        double Im { get; }
+        double Abs { get; }
+        double Arg { get; }
+    }
+}
